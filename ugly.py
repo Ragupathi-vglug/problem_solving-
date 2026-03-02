35f8b86ba750263d16603e41533dd8188c9a6810
MADARA143390 <ragu26555@gmail.com>
2026-03-02 22:23:04 +0530

    New files added ugly

---
+++ ugly.py
@@ -0,0 +1,6 @@
+ugly_num=int(input("Enter the number:"))
+
+if ugly_num%5==0 or ugly_num%3==0 or ugly_num%2==0:
+    print("The given number is ugly number")
+else:
+    print("The given number is not a ugly number")
