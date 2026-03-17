f7c542b0172dc372e16090f1de45b123993d9b7a
MADARA143390 <ragu26555@gmail.com>
2026-03-17 22:57:28 +0530

    Files modified

--- no_repeat.py
+++ no_repeat.py
@@ -1,11 +1,19 @@
-s=input("Enter the String :")
-# s="abca"
-for i in s:
-    if s.count(i)==1:
-        print("The non repeated value is :",i)
-        break
-# result=[]
+# s=input("Enter the String :")
+s="abca"
 # for i in s:
-#     if i==s[i]:
+#     if s.count(i)==1:
+#         print("The non repeated value is :",i)
+#         break
+result=[]
+# for i in range(len(s)):
+#     if not(s[i]==s[i+1] or s[i]==s[i-1]):
 #         result.append(s[i])
-# print("The not repeated value is :",result[0])
+#         break
+for i in s:
+    if i is not s:
+        result.append(i)
+        break
+if result is not None:
+    print("The not repeated value is :",result[0])
+else:
+    print("There is no repeating value Found!")
