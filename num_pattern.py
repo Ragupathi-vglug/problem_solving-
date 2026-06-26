74c61c3e627e5363552c482f59274913c6c832ea
MADARA143390 <ragu26555@gmail.com>
2026-06-26 20:46:51 +0530

    Fils added

---
+++ num_pattern.py
@@ -0,0 +1,15 @@
+a=int(input("Enter the number: "))
+for i in range(a+1):
+    p=1
+    for i in range(i):
+        print(p,end="")
+        p+=1
+    print("")
+
+a=int(input("Enter the number: "))
+for i in range(a+1):
+    p=a
+    for i in range(i):
+        print(p,end="")
+        p-=1
+    print("")
