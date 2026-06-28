54621b5b1fe35ea018726ef8ec331f1255ceac3d
Ragupathi_2k5 <ragu26555@gmail.com>
2026-06-28 16:56:48 +0530

    Clean up num_pattern.py by removing commented code
    Removed unused input and loop for printing decreasing pattern.

--- num_pattern.py
+++ num_pattern.py
@@ -5,11 +5,3 @@ for i in range(a+1):
         print(p,end="")
         p+=1
     print("")
-
-a=int(input("Enter the number: "))
-for i in range(a+1):
-    p=a
-    for i in range(i):
-        print(p,end="")
-        p-=1
-    print("")
