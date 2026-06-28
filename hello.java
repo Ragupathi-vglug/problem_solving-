dbfdd3c64646b36318066ab5f57c01b0aca03d66
MADARA143390 <ragu26555@gmail.com>
2026-06-28 22:14:39 +0530

    Files changes

--- hello.java
+++ hello.java
@@ -1,5 +1,19 @@
+import java.util.*;
 class hello{
     public static void main(String args[]){
-        System.out.println("Hello world !");
+        Scanner s=new Scanner(System.in);
+        int num;
+        System.out.print("Enter the number :");
+        num=s.nextInt();
+        System.out.println();
+        for(int i=1;i<=num;i++){
+            for(int space=num-i;space>0;space--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
